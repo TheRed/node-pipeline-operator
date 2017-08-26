e5d3fc9661e1d95f809e23459c81c93905dc2790
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-08-26 10:15:00 +0900

    サンプルコード作成

---
+++ index.js
@@ -0,0 +1,15 @@
+function doubleSay (str) {
+  return str + ", " + str;
+}
+function capitalize (str) {
+  return str[0].toUpperCase() + str.substring(1);
+}
+function exclaim (str) {
+  return str + '!';
+}
+
+"hello"
+  |> doubleSay
+  |> capitalize
+  |> exclaim
+  |> console.log
